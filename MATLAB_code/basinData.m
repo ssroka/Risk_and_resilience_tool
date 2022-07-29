a0d58cc1d609a31865eecc086027e8cbcc531ac9
Sydney Sroka <sgsroka@gmail.com>
2022-07-29 13:41:50 -0400

    edited basinData into a function that outputs a concatenated table

--- MATLAB_code/basinData.m
+++ MATLAB_code/basinData.m
@@ -1,175 +1,48 @@
-ans1 = readgeotable('SAU_C50010101.shp').Shape;
-ans2 = readgeotable('SAU_C50010102.shp').Shape;
-ans3 = readgeotable('SAU_C50010103.shp').Shape;
-ans4 = readgeotable('SAU_C50010104.shp').Shape;
-ans5 = readgeotable('SAU_C50010105.shp').Shape;
-ans6 = readgeotable('SAU_C50010106.shp').Shape;
-ans7 = readgeotable('SAU_C50010107.shp').Shape;
-ans8 = readgeotable('SAU_C50010108.shp').Shape;
-ans9 = readgeotable('SAU_C50010109.shp').Shape;
-ans10 = readgeotable('SAU_C50010110.shp').Shape;
-ans11 = readgeotable('SAU_C50010111.shp').Shape;
-ans12 = readgeotable('SAU_C50010112.shp').Shape;
-ans13 = readgeotable('SAU_C50010113.shp').Shape;
-ans14 = readgeotable('SAU_C50010114.shp').Shape;
-ans2_1 = readgeotable('SAU_C50020101.shp').Shape;
-ans2_2 = readgeotable('SAU_C50020102.shp').Shape;
+function [basinTable] = basinData()
 
-kb56_1 = readgeotable('C50560101.shp').Shape;
-kb56_2 = readgeotable('C50560102.shp').Shape;
-kb62_1 = readgeotable('SAU_C50620101.shp').Shape;
-kb62_2 = readgeotable('SAU_C50620102.shp').Shape;
-kb62_3 = readgeotable('SAU_C50620103.shp').Shape;
+% This function reads in shape files of saline basins and outputs a
+% concatenated table of all basins
 
-acp1 = readgeotable('SAU_C50700101.shp').Shape;
-acp2 = readgeotable('SAU_C50700102.shp').Shape;
+% The files are in the format SAU_CXXXXXXXX.shp
+% fn is a 1xn vector of all of the file numbers
 
-bb1 = readgeotable('SAU_C50340101.shp').Shape;
-bb2 = readgeotable('SAU_C50340102.shp').Shape;
-bb3 = readgeotable('SAU_C50340103.shp').Shape;
-bb4 = readgeotable('SAU_C50340104.shp').Shape;
-bb5 = readgeotable('SAU_C50340105.shp').Shape;
-bb6 = readgeotable('SAU_C50340106.shp').Shape;
-bb7 = readgeotable('SAU_C50340107.shp').Shape;
-bb8 = readgeotable('SAU_C50340108.shp').Shape;
-bb9 = readgeotable('SAU_C50340109.shp').Shape;
-bb10 = readgeotable('SAU_C50340110.shp').Shape;
-bb11 = readgeotable('SAU_C50340111.shp').Shape;
-bb12 = readgeotable('SAU_C50340112.shp').Shape;
+fn = horzcat(50010101:50010114,...
+    50560101:50560102,...
+    50020101:50020102,...
+    50620101:50620103,...
+    50700101:50700102,...
+    50340101:50340112,...
+    50040101,...
+    50050101:50050102,...
+    50390101:50390105,...
+    50680101:100:50680301,...
+    50360101:50360108,...
+    50370101:50370114,...
+    50370118:50370128,...
+    50490101:50490117,...
+    50300101:50300112,...
+    50430101:50430103,...
+    50440101:50440103,...
+    50450101:50450102,...
+    50330101:50330110,...
+    50500101:50500105,...
+    50190101:50190102,...
+    50200101:50200105,...
+    50210101,...
+    50220101:50220104,...
+    50410101,...
+    50310101:50310109);
 
-cb4 = readgeotable('SAU_C50040101.shp').Shape;
-cb5_1 = readgeotable('SAU_C50050101.shp').Shape;
-cb5_2 = readgeotable('SAU_C50050102.shp').Shape;
+N = length(fn);
 
-db1 = readgeotable('SAU_C50390101.shp').Shape;
-db2 = readgeotable('SAU_C50390102.shp').Shape;
-db3 = readgeotable('SAU_C50390103.shp').Shape;
-db4 = readgeotable('SAU_C50390104.shp').Shape;
-db5 = readgeotable('SAU_C50390105.shp').Shape;
+% initialize array
+basinCellArray = cell(N,1);
 
-emb1 = readgeotable('SAU_C50680101.shp').Shape;
-emb2 = readgeotable('SAU_C50680201.shp').Shape;
-emb3 = readgeotable('SAU_C50680301.shp').Shape;
+% populate cell array with shape files
+for i = 1:length(fn)
+    basinCellArray{i} = readgeotable(sprintf('SAU_C%d.shp',fn(i))).Shape;
+end
 
-ggrb36_1 = readgeotable('SAU_C50360101.shp').Shape;
-ggrb36_2 = readgeotable('SAU_C50360102.shp').Shape;
-ggrb36_3 = readgeotable('SAU_C50360103.shp').Shape;
-ggrb36_4 = readgeotable('SAU_C50360104.shp').Shape;
-ggrb36_5 = readgeotable('SAU_C50360105.shp').Shape;
-ggrb36_6 = readgeotable('SAU_C50360106.shp').Shape;
-ggrb36_7 = readgeotable('SAU_C50360107.shp').Shape;
-ggrb36_8 = readgeotable('SAU_C50360108.shp').Shape;
+% convert cell array to table
+basinTable = cell2table(basinCellArray);
 
-ggrb37_1 = readgeotable('SAU_C50370101.shp').Shape;
-ggrb37_2 = readgeotable('SAU_C50370102.shp').Shape;
-ggrb37_3 = readgeotable('SAU_C50370103.shp').Shape;
-ggrb37_4 = readgeotable('SAU_C50370104.shp').Shape;
-ggrb37_5 = readgeotable('SAU_C50370105.shp').Shape;
-ggrb37_6 = readgeotable('SAU_C50370106.shp').Shape;
-ggrb37_7 = readgeotable('SAU_C50370107.shp').Shape;
-ggrb37_8 = readgeotable('SAU_C50370108.shp').Shape;
-ggrb37_9 = readgeotable('SAU_C50370109.shp').Shape;
-ggrb37_10 = readgeotable('SAU_C50370110.shp').Shape;
-ggrb37_11 = readgeotable('SAU_C50370111.shp').Shape;
-ggrb37_12 = readgeotable('SAU_C50370112.shp').Shape;
-ggrb37_13 = readgeotable('SAU_C50370113.shp').Shape;
-ggrb37_14 = readgeotable('SAU_C50370114.shp').Shape;
-
-gb2_0 = readgeotable('SAU_C50470120.shp').Shape;
-gb2_1 = readgeotable('SAU_C50470121.shp').Shape;
-gb2_2 = readgeotable('SAU_C50470122.shp').Shape;
-gb2_3 = readgeotable('SAU_C50470123.shp').Shape;
-gb2_4 = readgeotable('SAU_C50470124.shp').Shape;
-gb2_5 = readgeotable('SAU_C50470125.shp').Shape;
-gb2_6 = readgeotable('SAU_C50470126.shp').Shape;
-gb2_7 = readgeotable('SAU_C50470127.shp').Shape;
-gb2_8 = readgeotable('SAU_C50470128.shp').Shape;
-gb1_8 = readgeotable('SAU_C50370118.shp').Shape;
-gb1_9 = readgeotable('SAU_C50370119.shp').Shape;
-
-gb0_1 = readgeotable('SAU_C50490101.shp').Shape;
-gb0_2 = readgeotable('SAU_C50490102.shp').Shape;
-gb0_3 = readgeotable('SAU_C50490103.shp').Shape;
-gb0_4 = readgeotable('SAU_C50490104.shp').Shape;
-gb0_5 = readgeotable('SAU_C50490105.shp').Shape;
-gb0_6 = readgeotable('SAU_C50490106.shp').Shape;
-gb0_7 = readgeotable('SAU_C50490107.shp').Shape;
-gb0_8 = readgeotable('SAU_C50490108.shp').Shape;
-gb0_9 = readgeotable('SAU_C50490109.shp').Shape;
-gb0_10 = readgeotable('SAU_C50490110.shp').Shape;
-gb0_11 = readgeotable('SAU_C50490111.shp').Shape;
-gb0_12 = readgeotable('SAU_C50490112.shp').Shape;
-gb0_13 = readgeotable('SAU_C50490113.shp').Shape;
-gb0_14 = readgeotable('SAU_C50490114.shp').Shape;
-gb0_17 = readgeotable('SAU_C50490115.shp').Shape;
-gb0_18 = readgeotable('SAU_C50490116.shp').Shape;
-gb0_19 = readgeotable('SAU_C50490117.shp').Shape;
-
-hlsb1 = readgeotable('SAU_C50300101.shp').Shape;
-hlsb2 = readgeotable('SAU_C50300102.shp').Shape;
-hlsb3 = readgeotable('SAU_C50300103.shp').Shape;
-hlsb4 = readgeotable('SAU_C50300104.shp').Shape;
-hlsb5 = readgeotable('SAU_C50300105.shp').Shape;
-hlsb6 = readgeotable('SAU_C50300106.shp').Shape;
-hlsb7 = readgeotable('SAU_C50300107.shp').Shape;
-hlsb8 = readgeotable('SAU_C50300108.shp').Shape;
-hlsb9 = readgeotable('SAU_C50300109.shp').Shape;
-hlsb10 = readgeotable('SAU_C50300110.shp').Shape;
-hlsb11 = readgeotable('SAU_C50300111.shp').Shape;
-hlsb12 = readgeotable('SAU_C50300112.shp').Shape;
-
-pp43_1 = readgeotable('SAU_C50430101.shp').Shape;
-pp43_2 = readgeotable('SAU_C50430102.shp').Shape;
-ppb43_3 = readgeotable('SAU_C50430103.shp').Shape;
-
-ppb44_1 = readgeotable('SAU_C50440101.shp').Shape;
-ppb44_2 = readgeotable('SAU_C50440102.shp').Shape;
-ppb44_3 = readgeotable('SAU_C50440103.shp').Shape;
-
-ppb45_1 = readgeotable('SAU_C50450101.shp').Shape;
-ppb45_2 = readgeotable('SAU_C50450102.shp').Shape;
-
-prb_1 = readgeotable('SAU_C50330101.shp').Shape;
-prb_2 = readgeotable('SAU_C50330102.shp').Shape;
-prb_3 = readgeotable('SAU_C50330103.shp').Shape;
-prb_4 = readgeotable('SAU_C50330104.shp').Shape;
-prb_5 = readgeotable('SAU_C50330105.shp').Shape;
-prb_6 = readgeotable('SAU_C50330106.shp').Shape;
-prb_7 = readgeotable('SAU_C50330107.shp').Shape;
-prb_8 = readgeotable('SAU_C50330108.shp').Shape;
-prb_9 = readgeotable('SAU_C50330109.shp').Shape;
-prb_10 = readgeotable('SAU_C50330110.shp').Shape;
-
-sfb1 = readgeotable('SAU_C50500101.shp').Shape;
-sfb2 = readgeotable('SAU_C50500102.shp').Shape;
-sfb3 = readgeotable('SAU_C50500103.shp').Shape;
-sfb4 = readgeotable('SAU_C50500104.shp').Shape;
-sfb5 = readgeotable('SAU_C50500105.shp').Shape;
-
-srb19_1 = readgeotable('SAU_C50190101.shp').Shape;
-srb19_2 = readgeotable('SAU_C50190102.shp').Shape;
-
-srb20_1 = readgeotable('SAU_C50200101.shp').Shape;
-srb20_2 = readgeotable('SAU_C50200102.shp').Shape;
-srb20_3 = readgeotable('SAU_C50200103.shp').Shape;
-srb20_4 = readgeotable('SAU_C50200104.shp').Shape;
-srb20_5 = readgeotable('SAU_C50200105.shp').Shape;
-
-srb21_1 = readgeotable('SAU_C50210101.shp').Shape;
-
-srb22_1 = readgeotable('SAU_C50220101.shp').Shape;
-srb22_2 = readgeotable('SAU_C50220102.shp').Shape;
-srb22_3 = readgeotable('SAU_C50220103.shp').Shape;
-srb22_4 = readgeotable('SAU_C50220104.shp').Shape;
-
-srb41_1 = readgeotable('SAU_C50410101.shp').Shape;
-
-wb_1 = readgeotable('SAU_C50310101.shp').Shape;
-wb_2 = readgeotable('SAU_C50310102.shp').Shape;
-wb_3 = readgeotable('SAU_C50310103.shp').Shape;
-wb_4 = readgeotable('SAU_C50310104.shp').Shape;
-wb_5 = readgeotable('SAU_C50310105.shp').Shape;
-wb_6 = readgeotable('SAU_C50310106.shp').Shape;
-wb_7 = readgeotable('SAU_C50310107.shp').Shape;
-wb_8 = readgeotable('SAU_C50310108.shp').Shape;
-wb_9 = readgeotable('SAU_C50310109.shp').Shape;
